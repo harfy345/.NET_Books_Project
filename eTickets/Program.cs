3befd97f0601e5f19f773c533dda8c5e4f5fdc37
harfy345 <hakam.almotlk345@gmail.com>
2023-12-21 15:27:10 -0500

    4

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,10 +1,11 @@
 using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 //Dbcontext configuration
-builder.Services.AddDbContext<AppDBContext>();
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
